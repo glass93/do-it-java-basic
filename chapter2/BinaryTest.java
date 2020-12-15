43c6790c154f9ec05e94b89d11e32254cb0e6aa3
3jung <72074545+glass93@users.noreply.github.com>
2020-12-15 22:43:57 +0900

    연습

---
+++ chapter2/BinaryTest.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class BinaryTest {
+	
+	public static void main(String[] args) {
+		
+		int num = 10;
+		int bnum = 0B1010;		// 10
+		int onum = 012;			// 10
+		int hnum = 0Xa;			// 10
+		
+		System.out.println(num);
+		System.out.println(bnum);
+		System.out.println(onum);
+		System.out.println(hnum);
+		
+	}
+
+}
---
+++ chapter2/TypeInference.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class TypeInference {
+	public static void main(String[] args) {
+		var i = 10;			// int i = 10으로 컴파일됨
+		var j = 10.0;		// double j = 10.0으로 컴파일됨
+		var str = "hello";	// String str = "hello"로 컴파일됨
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(str);
+		
+		i = 35;					// 다른 정수값은 대입 가능
+		System.out.println(i);
+		
+		str = "test";			// 다른 문자열은 대입 가능
+		// str = 3;				   str 변수는 String형으로 먼저 사용되었기 때문에 정수 값을 넣을 수 없음
+		System.out.println(str);
+	}
+
+}
