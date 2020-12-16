cf28905cda7bd9926a7d19e38240e58d300394ab
3jung <72074545+glass93@users.noreply.github.com>
2020-12-16 19:25:34 +0900

    연습

---
+++ chapter4/IfExample2.java
@@ -0,0 +1,14 @@
+package ifexample;
+
+public class IfExample2 {
+	public static void main(String[] args) {
+		char gender = 'F';
+		if(gender == 'F') {
+			System.out.println("여자입니다.");
+		}
+		else {
+			System.out.println("남자입니다."); 
+		}
+	}
+
+}
